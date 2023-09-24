c9ba6351069d75740c54958448153df482a9ffbe
bagir-kaff <trexhilang@gmail.com>
2023-09-24 13:09:32 +0330

    Put html element to variables

--- external.js
+++ external.js
@@ -1,13 +1,16 @@
+const input = document.querySelector('input[class=operation]')
+const resultPanel = document.querySelector('.result')
+
 function Add(x,y){
-    return x+y
+  return x+y
 }
 function Substract(x,y){
-    return x-y
+  return x-y
 }
 function Multiply(x,y){
-    return x*y
+  return x*y
 }
 function Divide(x,y){
-    return x/y
+  return x/y
 }
 
