239d32794488e96151ce1d50a6005f21e719a0f9
bagir-kaff <trexhilang@gmail.com>
2023-10-10 14:57:49 +0330

    Put semicolon at the end of each line

--- external.js
+++ external.js
@@ -1,11 +1,11 @@
-const NumberButtons = document.querySelectorAll('.button.number')
-const objectPanel = document.querySelector('.object')
-const resultPanel = document.querySelector('.result')
-const clearButton = document.querySelector('.clear')
-const equalButton = document.querySelector('.equal')
-const operatorButtons = document.querySelectorAll('.operator')
-const decimalButton = document.querySelector('.decimal')
-const backspaceButton = document.querySelector('.backspace')
+const NumberButtons = document.querySelectorAll('.button.number');
+const objectPanel = document.querySelector('.object');
+const resultPanel = document.querySelector('.result');
+const clearButton = document.querySelector('.clear');
+const equalButton = document.querySelector('.equal');
+const operatorButtons = document.querySelectorAll('.operator');
+const decimalButton = document.querySelector('.decimal');
+const backspaceButton = document.querySelector('.backspace');
 let objects = ["","",""];
 let isFirst = true;
 NumberButtons.forEach(number => {
@@ -22,7 +22,7 @@ NumberButtons.forEach(number => {
 });
 operatorButtons.forEach(operator =>{
   operator.addEventListener('click',()=>{
-    decimalButton.disabled = false
+    decimalButton.disabled = false;
     objects[0] = Operate(objects[0],objects[2],objects[1]);
     if(objects[2]===''){
       isFirst = false;
@@ -30,41 +30,41 @@ operatorButtons.forEach(operator =>{
     else{
       objects[2] = '';
     }
-    objects[1] = operator.textContent
-    objectPanel.textContent = objects[0]+objects[1]
+    objects[1] = operator.textContent;
+    objectPanel.textContent = objects[0]+objects[1];
     resultPanel.textContent = objects[0];
   })
 })
 backspaceButton.addEventListener('click',()=>{
   if(objects[2]!==''){
-    objects[2] = objects[2].slice(0,objects[2].length-1)
+    objects[2] = objects[2].slice(0,objects[2].length-1);
     }
   else if(objects[1]!==''){
     objects[1]='';
     isFirst=true;
   }
   else if(objects[0]!==''){
-    objects[0] = objects[0].slice(0,objects[0].length-1)
+    objects[0] = objects[0].slice(0,objects[0].length-1);
   }
-  resultPanel.textContent = Operate(objects[0],objects[2],objects[1])
+  resultPanel.textContent = Operate(objects[0],objects[2],objects[1]);
   objectPanel.textContent = objects[0]+objects[1]+objects[2];
 })
 function Operate(first,second,oper){
   if(first===''){
-    return '0'
+    return '0';
   }
   else if(second === ''){
-    return first
+    return first;
   }
   switch (oper){
     case '+':
-      return Add(+first,+second)
+      return Add(+first,+second);
     case '-':
-      return Substract(first,second)
+      return Substract(first,second);
     case 'x':
-      return Multiply(first,second)
+      return Multiply(first,second);
     case '/':
-      return Divide(first,second)
+      return Divide(first,second);
   }
 }
 equalButton.addEventListener('click',()=>{
@@ -78,54 +78,55 @@ equalButton.addEventListener('click',()=>{
     objectPanel.textContent = Operate(objects[0],objects[2],objects[1]);
     // isFirst=true;
     // objects[0] = '', objects[2] = '', objects[1]='';
+    //if enabled, will reset objects
   }
 })
 
 function Clear(){
   isFirst=true;
   objectPanel.textContent = '---';
-  resultPanel.textContent = '---'
+  resultPanel.textContent = '---';
   objects[0] = '', objects[2] = '', objects[1]='';
 }
 clearButton.addEventListener('click' ,Clear)
 
 decimalButton.addEventListener('click',()=>{
-  decimalButton.disabled = true
+  decimalButton.disabled = true;
   if(objects[2].search(/\./)===-1&&objects[1]!==''){
     if(objects[2]==='')
-      objects[2]+='0'
-    objects[2]+='.'
+      objects[2]+='0';
+    objects[2]+='.';
   }
   else if (objects[0].search(/\./)===-1&&objects[1]===''){
     if(objects[0]==='')
-      objects[0]+='0'
+      objects[0]+='0';
     objects[0]+='.';
   }
-  resultPanel.textContent = Operate(objects[0],objects[2],objects[1])
+  resultPanel.textContent = Operate(objects[0],objects[2],objects[1]);
   objectPanel.textContent = objects[0]+objects[1]+objects[2];
 })
 function Add(x,y){
-  return x+y
+  return x+y;
 }
 function Substract(x,y){
-  return x-y
+  return x-y;
 }
 function Multiply(x,y){
-  return x*y
+  return x*y;
 }
 function Divide(x,y){
   if(x>0&&+y===0){
-    Clear()
-    return alert('why!! its infinity ')
+    Clear();
+    return alert('why!! its infinity ');
   }
   else if (x<0&&+y===0){
-    Clear()
-    return alert('whyy! its negative infinity')
+    Clear();
+    return alert('whyy! its negative infinity');
   }
   else if(+x===0 && +y===0){
-    Clear()
-    return alert('what is this? is it equal to one?')
+    Clear();
+    return alert('what is this? is it equal to one?');
   }
-  return x/y
+  return x/y;
 }
 
