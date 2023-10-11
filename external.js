28e4b67ee94b7f811f161e7a45c9b0d97aabf975
bagir-kaff <trexhilang@gmail.com>
2023-10-11 23:43:08 +0330

    Enable keyboard supports for all features
    and in order to do that, some of the existing functions need are
    modified to take parameter from keys;
    secondly, modified to make equal function reset objectPanel, makes it more
    predictable;
    third, Operate function return a string to enable operation with
    exponents;
    fourth, divided and cleaned code, by variable, event, and function.

--- external.js
+++ external.js
@@ -8,50 +8,37 @@ const decimalButton = document.querySelector('.decimal');
 const backspaceButton = document.querySelector('.backspace');
 let objects = ["","",""];
 let isFirst = true;
-NumberButtons.forEach(number => {
-  number.addEventListener('click',()=>{
-    if(isFirst){
-      objects[0] +=number.textContent;
-    }
-    else{
-      objects[2] +=number.textContent;
-    }
-    
-    objectPanel.textContent = objects[0]+objects[1]+objects[2];
-    resultPanel.textContent = Operate(objects[0],objects[2],objects[1]);
-  })
-});
-operatorButtons.forEach(operator =>{
-  operator.addEventListener('click',()=>{
-    decimalButton.disabled = false;
-    objects[0] = Operate(objects[0],objects[2],objects[1]);
-    if(objects[2]===''){
-      isFirst = false;
-    }
-    else{
-      objects[2] = '';
-    }
-    objects[1] = operator.textContent;
-    objectPanel.textContent = objects[0]+objects[1];
-    resultPanel.textContent = objects[0];
-  })
-})
-backspaceButton.addEventListener('click',()=>{
-  if(objects[2]!==''){
-    objects[2] = objects[2].slice(0,objects[2].length-1);
-    }
-  else if(objects[1]!==''){
-    objects[1]='';
-    isFirst=true;
+
+//Keyboard Supports :D
+window.addEventListener('keydown',(e)=>{
+  if(e.key ==='1'||e.key ==='2'||e.key ==='3'
+  ||e.key ==='4'||e.key ==='5'||e.key ==='6'
+  ||e.key ==='7'||e.key ==='8'||e.key ==='9'||e.key==='0'){
+    AddNumbers(e.key);
   }
-  else if(objects[0]!==''){
-    objects[0] = objects[0].slice(0,objects[0].length-1);
+  else if(e.key === '+'||e.key === '-'||e.key === '*'
+  ||e.key==='x'||e.key === '/'||e.key ==='X'){
+    if(e.key==='/'){
+      e.preventDefault();
+    }
+    AddOperators(e.key);
   }
-  resultPanel.textContent = Operate(objects[0],objects[2],objects[1]);
-  objectPanel.textContent = objects[0]+objects[1]+objects[2];
-  if(objects[0]==='')
-    objectPanel.textContent='0'
+  else if(e.key ==='.'){
+    AddDecimal()
+  }
+  else if(e.key === 'c'||e.key === 'C'){
+    Clear();
+  }
+  else if(e.key === '='){
+    Equal()
+  }
+  else if(e.key === 'Backspace'){
+    Backspace()
+  }
 })
+NumberButtons.forEach(number => number.addEventListener('click', AddNumbers));
+operatorButtons.forEach(operator =>operator.addEventListener('click', AddOperators))
+backspaceButton.addEventListener('click',Backspace)
 function Operate(first,second,oper){
   if(first===''){
     return '0';
@@ -61,53 +48,22 @@ function Operate(first,second,oper){
   }
   switch (oper){
     case '+':
-      return Add(+first,+second);
+      return String(Add(+first,+second));
     case '-':
-      return Substract(first,second);
+      return String(Substract(first,second));
     case 'x':
-      return Multiply(first,second);
+    case '*':
+    case 'X':
+      return String(Multiply(first,second));
     case '/':
-      return Divide(first,second);
+      return String(Divide(first,second));
   }
 }
-equalButton.addEventListener('click',()=>{
-  if(objects[0]==='')
-    alert('the first number is empty');
-  else if(objects[1] ==='')
-    alert('no operator is chosen');
-  else if(objects[2]==='')
-    alert('the second number is empty');
-  else{
-    objectPanel.textContent = Operate(objects[0],objects[2],objects[1]);
-    isFirst=true;
-    objects[0] = '', objects[2] = '', objects[1]='';
-    //if enabled, will reset objects
-  }
-})
+equalButton.addEventListener('click',Equal)
 
-function Clear(){
-  isFirst=true;
-  objectPanel.textContent = '---';
-  resultPanel.textContent = '---';
-  objects[0] = '', objects[2] = '', objects[1]='';
-}
 clearButton.addEventListener('click' ,Clear)
 
-decimalButton.addEventListener('click',()=>{
-  decimalButton.disabled = true;
-  if(objects[2].search(/\./)===-1&&objects[1]!==''){
-    if(objects[2]==='')
-      objects[2]+='0';
-    objects[2]+='.';
-  }
-  else if (objects[0].search(/\./)===-1&&objects[1]===''){
-    if(objects[0]==='')
-      objects[0]+='0';
-    objects[0]+='.';
-  }
-  resultPanel.textContent = Operate(objects[0],objects[2],objects[1]);
-  objectPanel.textContent = objects[0]+objects[1]+objects[2];
-})
+decimalButton.addEventListener('click',AddDecimal)
 function Add(x,y){
   return x+y;
 }
@@ -132,4 +88,83 @@ function Divide(x,y){
   }
   return x/y;
 }
+
+function AddNumbers(number){
+  if((number.type)==='click'){
+    number=this.textContent }
+  if(isFirst){
+    objects[0] +=number;
+  }
+  else{
+    objects[2] +=number;
+  }
+  objectPanel.textContent = objects[0]+objects[1]+objects[2];
+  resultPanel.textContent = Operate(objects[0],objects[2],objects[1]);
+}
+function AddOperators(operator){
+  if((operator.type)==='click'){
+    operator=this.textContent }
+  decimalButton.disabled = false;
+  objects[0] = Operate(objects[0],objects[2],objects[1]);
+  if(objects[2]===''){
+    isFirst = false;
+  }
+  else{
+    objects[2] = '';
+  }
+  objects[1] = operator;
+  objectPanel.textContent = objects[0]+objects[1];
+  resultPanel.textContent = objects[0];
+}
+function Equal(){
+  if(objects[0]==='')
+    alert('the first number is empty');
+  else if(objects[1] ==='')
+    alert('no operator is chosen');
+  else if(objects[2]==='')
+    alert('the second number is empty');
+  else{
+    objectPanel.textContent = '---';
+    isFirst=true;
+    objects[0] = '', objects[2] = '', objects[1]='';
+    //if enabled, will reset objects
+  }
+}
+function Clear(){
+  isFirst=true;
+  objectPanel.textContent = '---';
+  resultPanel.textContent = '---';
+  objects[0] = '', objects[2] = '', objects[1]='';
+}
+function Backspace(){
+    if(objects[2]!==''){
+      objects[2] = objects[2].slice(0,objects[2].length-1);
+      }
+    else if(objects[1]!==''){
+      objects[1]='';
+      isFirst=true;
+    }
+    else if(objects[0]!==''){
+      objects[0] = objects[0].slice(0,objects[0].length-1);
+    }
+    resultPanel.textContent = Operate(objects[0],objects[2],objects[1]);
+    objectPanel.textContent = objects[0]+objects[1]+objects[2];
+    if(objects[0]==='')
+      objectPanel.textContent='0'
+  }
+function AddDecimal(){
+    decimalButton.disabled = true;
+    if(objects[2].search(/\./)===-1&&objects[1]!==''){
+      if(objects[2]==='')
+        objects[2]+='0';
+      objects[2]+='.';
+    }
+    else if (objects[0].search(/\./)===-1&&objects[1]===''){
+      if(objects[0]==='')
+        objects[0]+='0';
+      objects[0]+='.';
+    }
+    resultPanel.textContent = Operate(objects[0],objects[2],objects[1]);
+    objectPanel.textContent = objects[0]+objects[1]+objects[2];
+  }
 
